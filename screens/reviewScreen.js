c8243e99b4e0778dbe3d69189d1aff1afd69ebf9
cassaram09 <cassaram09@gmail.com>
2018-02-01 09:50:48 -0500

    add basic screens

---
+++ screens/reviewScreen.js
@@ -0,0 +1,20 @@
+import React { Component } from 'react';
+import { Text, View } from 'react-native';
+
+class ReviewScreen extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>ReviewScreen</Text>
+        <Text>ReviewScreen</Text>
+        <Text>ReviewScreen</Text>
+        <Text>ReviewScreen</Text>
+        <Text>ReviewScreen</Text>
+        <Text>ReviewScreen</Text>
+        <Text>ReviewScreen</Text>
+      </View>
+    );
+  }
+}
+
+export default ReviewScreen;
